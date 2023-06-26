f165ff08fae7a50c9a3e06622dbfa57610047633
MD2990 <31358358+MD2990@users.noreply.github.com>
2023-06-26 14:34:09 +0400

    added req elements

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+const projectData = {};
 
 // Require Express to run server and routes
 const express = require("express");
@@ -21,9 +21,46 @@ app.use(cors());
 // Initialize the main project folder
 app.use(express.static("website"));
 
+app.get("/getData", function (req, res, next) {
+  try {
+    if (res.statusCode == 200) {
+      return res.send(projectData);
+    } else {
+      res.status(404).send("No Data Found ...");
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Post Route
+app.post("/add", function (req, res, next) {
+  try {
+    if (!req.body.temp || !req.body.date || !req.body.feelings) {
+      return res.status(400).send("Bad Request");
+    }
+
+    const { temp, date, feelings, icon } = req.body;
+
+    projectData.temp = temp;
+    projectData.date = date;
+    projectData.feelings = feelings;
+    projectData.icon = icon;
+    res.status(200).json({ done: true });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
 // Setup Server
 const port = 8000;
-const server = app.listen(port, listening);
+app.listen(port, listening);
 function listening() {
   console.log("server running");
   console.log(`running on localhost: ${port}`);
--- website/app.js
+++ website/app.js
@@ -1,5 +1,157 @@
 /* Global Variables */
 
+const units = "imperial";
+const apiKey = `bdbd673eca32ffdfc4607192143df538`;
+const baseURL = `http://api.openweathermap.org/data/2.5/weather?zip=`;
+
 // Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+const d = new Date();
+const newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+
+const generate = document.getElementById("generate");
+const zip = document.getElementById("zip");
+const feelings = document.getElementById("feelings");
+const date = document.getElementById("date");
+const temp = document.getElementById("temp");
+const content = document.getElementById("content");
+const miniIcon = document.getElementById("icn");
+
+// on DOM load
+document.addEventListener("DOMContentLoaded", function () {
+  generate.classList.add("disabled");
+  generate.addEventListener("click", performAction);
+
+  // set cursor disabled generate button
+
+  // disable the generate button if zip is empty
+  zip.addEventListener("keyup", function () {
+    //document.getElementById("generate").disabled = !this.value;
+    if (this.value.length < 3) {
+      generate.classList.add("disabled");
+    } else {
+      generate.classList.remove("disabled");
+    }
+  });
+});
+
+function performAction(e) {
+  e.preventDefault();
+
+  generate.textContent = "Loading ...";
+  const zipCode = zip.value;
+  const feeling = feelings.value;
+
+  if (!zipCode) {
+    alert("Please enter a zip code");
+    return;
+  }
+  getWeather()
+    .then(function (data) {
+      if (!data) {
+        return;
+        // if no data found, we will not post anything to the server , the cleanFields() function will be called
+        // from the getWeather function
+      }
+      postData("/add", {
+        temp: data?.main?.temp,
+        date: newDate,
+        feelings: feeling.length ? feeling : "No Feelings",
+        icon: data?.weather[0]?.icon,
+      });
+    })
+    .then(() => updateUI())
+    .catch(function (error) {
+      cleanFields();
+      console.log(error);
+    })
+    .finally(function () {
+      generate.textContent = "Generate";
+    });
+}
+
+const getWeather = async () => {
+  const res = await fetch(
+    `${baseURL}${zip.value}&appid=${apiKey}&units=${units}`
+  );
+
+  try {
+    if (!res.ok) {
+      alert("Please enter a valid zip code");
+      cleanFields();
+      return;
+    }
+    const data = await res.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    cleanFields();
+    console.log("error", error);
+  }
+};
+
+// update UI
+const updateUI = async () => {
+  const request = await fetch("/getData");
+  try {
+    const allData = await request.json(); // if no data found, set empty object
+    if (!allData || !Object.keys(allData).length) {
+      alert("No Data Found ...");
+      cleanFields();
+      return;
+    }
+
+    setValues(allData);
+  } catch (error) {
+    cleanFields();
+    console.log("error", error);
+  }
+};
+
+// postData
+const postData = async (url, data) => {
+  if (!data || !Object.keys(data).length || !url) {
+    alert("No Data Found ...");
+    cleanFields();
+    return;
+  }
+
+  const response = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  try {
+    if (!response.ok) {
+      alert("Please enter a valid zip code");
+      zip.value = "";
+      cleanFields();
+      return;
+    }
+
+    const newData = await response.json();
+    return newData;
+  } catch (error) {
+    cleanFields();
+    console.log("error", error);
+  }
+};
+
+function setValues(allData) {
+  const { temp: temps, date: dates, feelings, icon } = allData;
+  date.textContent = dates;
+  temp.textContent = Math.round(temps) + "°";
+  content.textContent = feelings;
+  miniIcon.src = `http://openweathermap.org/img/w/${icon}.png`;
+}
+
+// clean fields on error
+function cleanFields() {
+  date.textContent = "";
+  temp.textContent = "";
+  content.textContent = "";
+  zip.value = "";
+  feelings.value = "";
+}
